f344ef88cf0fe5c836842f9c22e469f8a24d2b4a
Leonel <leonelaf12@gmail.com>
2019-04-09 16:56:10 -0400

    added five events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+
+//Using mouseOver to change color of main heading 
+const fbHeading = document.querySelector('.logo-heading');
+fbHeading.addEventListener('mouseover', function(event){
+    let target = event.target;
+    target.style.color = 'yellow';
+    
+    target.onmouseout = function(event){
+        target.style.color = '';
+    }
+});
+
+const nav = document.querySelector('.main-navigation');
+window.addEventListener('scroll', function(event){
+    nav.style.background = 'gray';
+});
+
+const intro = document.querySelector( '.intro h2');
+
+window.addEventListener('keydown', function(event){
+    intro.style.color = 'yellow';
+});
+
+window.addEventListener('keyup', function(event){
+    intro.style.color = '';
+});
+
+const introImg = document.querySelector('.intro img');
+introImg.addEventListener('dblclick', function(event){
+    let target = event.target;
+    target.style.border = '1px solid red';
+})
